b2f9bf4fe1a821edc5867862a0605eedbb1b76f2
Zane Sterling <sterling.zane@gmail.com>
2015-02-03 20:36:09 -0500

    Add sliders to change simulation

--- switchy-spiral/script.js
+++ switchy-spiral/script.js
@@ -1,4 +1,50 @@
-window.onload = function() {
+$(function() {
+	var slider_data = [
+		{
+			controls: "SEG_THICKNESS",
+			value: 30,
+			min: 1,
+			max: 100,
+			step: 1
+		},
+		{
+			controls: "SQUARE_RAD",
+			value: 4,
+			min: 1,
+			max: 10,
+			step: 0.1
+		},
+		{
+			controls: "ROT_SPEED",
+			value: 0.01,
+			min: 0.001,
+			max: 0.015,
+			step: 0.0001
+		},
+		{
+			controls: "NUM_ARMS",
+			value: 8,
+			min: 1,
+			max: 32,
+			step: 1
+		}
+	];
+	var sliders = $(".slider");
+	sliders.map(
+		function(index) {
+			var dat = slider_data[index];
+			$(sliders[index]).slider({
+				value: dat.value,
+				min: dat.min,
+				max: dat.max,
+				step: dat.step,
+				slide: function(event, ui) {
+					setVar(dat.controls, ui.value);
+				},
+			});
+		}
+	);
+
 	// canvas
 	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext("2d");
@@ -7,10 +53,12 @@ window.onload = function() {
 	ctx.fillStyle = "rgb(0,0,0)";
 	ctx.save();
 
-	var SEG_THICKNESS = 30;
-	var ROT_SPEED = 0.01;
-	var NUM_ARMS = 8;
-	var SQUARE_RAD = 4;
+	var data = {
+		SEG_THICKNESS: 30,
+		SQUARE_RAD:    4,
+		ROT_SPEED:     0.01,
+		NUM_ARMS:      8,
+	};
 
 	var rot = 0;
 
@@ -21,9 +69,9 @@ window.onload = function() {
 	};
 
 	var render = function(rot) {
-		for (var i = 0; i < NUM_ARMS; i++) {
+		for (var i = 0; i < data.NUM_ARMS; i++) {
 			drawArm(rot);
-			ctx.rotate(Math.PI * 2 / NUM_ARMS);
+			ctx.rotate(Math.PI * 2 / data.NUM_ARMS);
 		}
 	};
 
@@ -31,25 +79,29 @@ window.onload = function() {
 		ctx.save();
 		for (var i = 0; i < 8; i++) {
 			ctx.rotate(rot);
-			ctx.translate(SEG_THICKNESS, 0);
+			ctx.translate(data.SEG_THICKNESS, 0);
 			drawSegment();
 		}
 		ctx.restore();
 	};
 
 	var drawSegment = function() {
-		var x = SEG_THICKNESS;
-		ctx.fillRect(x - SQUARE_RAD, -SQUARE_RAD,
-	             	 2 * SQUARE_RAD, 2 * SQUARE_RAD);
+		var x = data.SEG_THICKNESS;
+		ctx.fillRect(x - data.SQUARE_RAD, -data.SQUARE_RAD,
+	             	 2 * data.SQUARE_RAD, 2 * data.SQUARE_RAD);
 	};
 
 	var tick = function() {
 		renderClear();
 		render(rot);
 		render(-rot);
-		rot += ROT_SPEED;
+		rot += data.ROT_SPEED;
 		setTimeout(tick, 20);
-	}
+	};
+
+	var setVar = function(k, v) {
+		data[k] = v;
+	};
 
 	tick();
-};
+});
