17f20aa7c8fecc476c49af6fa88225cfc45e869d
Zane Sterling <sterling.zane@gmail.com>
2015-05-15 19:09:09 -0400

    Add spikes

--- triangle-folding/script.js
+++ triangle-folding/script.js
@@ -29,22 +29,51 @@
 		drawTri(-Math.sqrt(3) / 2 * sideLen * Math.cos(theta), sideLen);
 	};
 
+	var drawSpike = function(sideLen) {
+		ctx.save();
+
+		ctx.translate(0, sideLen);
+		ctx.rotate(2 * Math.PI / 6);
+		drawEquiTri(sideLen);
+
+		ctx.restore();
+	};
+
+	var drawFlippingSpike = function(sideLen, t) {
+		ctx.save();
+
+		ctx.translate(0, sideLen);
+		ctx.rotate(2 * Math.PI / 3);
+		drawFlippingTri(sideLen, t);
+
+		ctx.restore();
+	};
+
 	var tick = function(t) {
+		var ts = t % CYCLE_LEN;
 		return function() {
 			ctx.clearRect(-w/2, -h/2, w, h);
 
+			// rotate appropriate number of cycles
 			if (t % CYCLE_LEN == 0) {
 				ctx.rotate(-2 * Math.PI / 6);
 			}
 
-			drawFlippingTri(SIDE_LEN, t);
-			for (var i = 0; i < 5; i++) {
+			// draw centerpiece
+			drawFlippingTri(SIDE_LEN, ts);
+			for (var i = 0; i < 4; i++) {
 				drawEquiTri(SIDE_LEN);
+				if (i == 1)
+					drawFlippingSpike(SIDE_LEN, ts);
+				else if (i == 2)
+					drawFlippingSpike(SIDE_LEN, (CYCLE_LEN - ts) % CYCLE_LEN);
+
 				ctx.rotate(2 * Math.PI / 6);
 			}
-			drawFlippingTri(SIDE_LEN, t);
-			ctx.rotate(2 * Math.PI / 6);
+			drawFlippingTri(SIDE_LEN, ts);
+			ctx.rotate(2 * Math.PI / 3);
 
+			// restore context and prepare for next call
 			ctx.restore();
 			setTimeout(tick(t+1), 30);
 		};
