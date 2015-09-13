faf71e1f2e36bca66b65aaa6277ceab2743ba671
Zane Sterling <sterling.zane@gmail.com>
2015-09-13 12:39:05 -0400

    Add more sphincters

--- inhaling-circles/script.js
+++ inhaling-circles/script.js
@@ -6,8 +6,9 @@ var init = function() {
 		rad: 300,
 		offset: 100,
 		numCircles: 10,
-		cycle: 600,
+		cycle: 2000,
 		t: 0,
+		rot: 0,
 	};
 
 	loop(state);
@@ -21,7 +22,7 @@ var loop = function(state) {
 	update(state);
 	render(state);
 
-	setTimeout(function() { loop(state); }, 30);
+	setTimeout(function() { loop(state); }, 20);
 };
 
 var update = function(state) {
@@ -39,10 +40,51 @@ var render = function(state) {
 	ctx.save();
 	ctx.translate(canvas.width / 2, canvas.height / 2);
 	var fractionThrough = state.t / state.cycle * 2*Math.PI;
+	ctx.rotate(fractionThrough * 4);
 	var baseRad = state.rad * Math.sin(Math.sin(fractionThrough) * 2*Math.PI);
 
 	ctx.lineWidth = 2;
 	ctx.strokeStyle = "#fff";
+
+	var centerRad = Math.sin(fractionThrough) * 100 + 20;
+
+	ctx.translate(-centerRad, 0);
+	for (var i = 0; i < state.numCircles; i++) {
+		var rad = Math.pow(
+			baseRad * (i + 1) / state.numCircles,
+			2
+		) / state.rad;
+
+		ctx.beginPath();
+		ctx.arc(0, 0, rad, 0, 2 * Math.PI);
+		ctx.stroke();
+	}
+
+	ctx.translate(centerRad * 2, 0);
+	for (var i = 0; i < state.numCircles; i++) {
+		var rad = Math.pow(
+			baseRad * (i + 1) / state.numCircles,
+			2
+		) / state.rad;
+
+		ctx.beginPath();
+		ctx.arc(0, 0, rad, 0, 2 * Math.PI);
+		ctx.stroke();
+	}
+
+	ctx.translate(-centerRad, centerRad);
+	for (var i = 0; i < state.numCircles; i++) {
+		var rad = Math.pow(
+			baseRad * (i + 1) / state.numCircles,
+			2
+		) / state.rad;
+
+		ctx.beginPath();
+		ctx.arc(0, 0, rad, 0, 2 * Math.PI);
+		ctx.stroke();
+	}
+
+	ctx.translate(0, -centerRad * 2);
 	for (var i = 0; i < state.numCircles; i++) {
 		var rad = Math.pow(
 			baseRad * (i + 1) / state.numCircles,
